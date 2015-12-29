7db4ca1e77ae06e9a751fe81f6bdb2f2dff59d8c
lapisdecor <lapisdecor@gmail.com>
2015-12-29 10:15:40 +0000

    reformat code to comply with pep8

--- bzoing.py
+++ bzoing.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import datetime
+
 
 class Task:
     def __init__(self, taskId, taskDesc):
@@ -8,22 +11,36 @@ class Task:
         self.dateCreated = datetime.datetime.now()
         self.alarmDate = None
 
-
-    def setTaskDesc(self, taskDesc):
+    def set_task_desc(self, taskDesc):
         self.taskDesc = taskDesc
 
-    def getTaskID(self):
+    def get_task_id(self):
         return self.taskId
 
-    def getTaskDesc(self):
+    def get_task_desc(self):
         return self.taskDesc
 
-    def getCreationDate(self):
+    def get_creation_date(self):
         return self.dateCreated
 
 
+class TaskList:
+    def __init__(self):
+        self.task_list = []
 
+    def add_task(self, task):
+        self.task_list.append(task)
+
+    def show_list(self):
+        for task in self.task_list:
+            print task.get_task_id(), task.get_creation_date(), task.get_task_desc()
 
 
 tarefa = Task(0, "Comprar Leite")
-print tarefa.getTaskID(), tarefa.getCreationDate(), tarefa.getTaskDesc()
+tarefa.set_task_desc("Lavar a loiça")
+tarefa2 = Task(1, "Comprar tabaco")
+
+lista = TaskList()
+lista.add_task(tarefa)
+lista.add_task(tarefa2)
+lista.show_list()
