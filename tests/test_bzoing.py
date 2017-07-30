aeb69479a6bf5492411e82bbcb77331daa8da819
lapisdecor <lapisdecor@gmail.com>
2017-07-30 14:12:01 +0100

    add a test to test the monitor

--- tests/test_bzoing.py
+++ tests/test_bzoing.py
@@ -10,6 +10,7 @@ from bzoing.tasks import Bzoinq, Monitor
 
 import time
 
+
 class TestTasksAndMonitor(unittest.TestCase):
 
     def test_creating_task(self):
@@ -24,6 +25,19 @@ class TestTasksAndMonitor(unittest.TestCase):
         a.remove_task(the_id)
         self.assertTrue(len(a.task_list) == 0)
 
+    def test_monitor(self):
+        import datetime
+        a = Bzoinq()
+        b = Monitor(a)
+        b.start()
+        first_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
+        a.create_task("My test task", alarm=first_time)
+        # sleep a bit to see if alarm works
+        time.sleep(15)
+        # check the if task was removed from task list
+        self.assertTrue(len(a.task_list) == 0)
+        # kill Monitor
+        b.stop()
 
 
 if __name__ == '__main__':
