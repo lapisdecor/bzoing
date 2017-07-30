a0fa6d4bc4ac76bc87de96222917f2374702544f
lapisdecor <lapisdecor@gmail.com>
2017-07-30 03:11:32 +0100

    add some unitest tests

--- tests/test_bzoing.py
+++ tests/test_bzoing.py
@@ -3,3 +3,28 @@ test_bzoing
 ----------------------------------
 Tests for `bzoing` module.
 """
+
+import unittest
+
+from bzoing.tasks import Bzoinq, Monitor
+
+import time
+
+class TestTasksAndMonitor(unittest.TestCase):
+
+    def test_creating_task(self):
+        a = Bzoinq()
+        a.create_task()
+        self.assertTrue(len(a.task_list) == 1)
+
+    def test_delete_task(self):
+        a = Bzoinq()
+        a.create_task()
+        the_id = a.task_list[0].id
+        a.remove_task(the_id)
+        self.assertTrue(len(a.task_list) == 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
