08edf9e7dac915ac37d8856c4c3a938ad2279cdb
lapisdecor <lapisdecor@gmail.com>
2017-08-18 11:26:57 +0100

    handle shutdown

--- bzoing/bzoing.py
+++ bzoing/bzoing.py
@@ -3,14 +3,16 @@
 from bzoing.tasks import Bzoinq, Monitor
 import time
 import gi
-
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from pkg_resources import resource_filename
-
 from . import share
+import signal
+import sys
+
 
 filepath = resource_filename(__name__, 'images/' + "sinoamarelo.svg")
+
 
 class Gui(Gtk.Window):
     def __init__(self):
@@ -24,7 +26,21 @@ class Gui(Gtk.Window):
         self.destroy()
         Gtk.main_quit()
 
+
+def handler(signum = None, frame = None):
+    if len(share.tasklist.get_task_list()) > 0:
+        with open('outfile.p', 'wb') as fp:
+                    pickle.dump(share.tasklist, fp)
+    print("Tasks have been saved")
+    sys.exit(0)
+
+
 def start():
+    # catch shutdown
+    for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+        signal.signal(sig, handler)
+
+
     # start the tasklist (Bzoinq)
     share.tasklist = Bzoinq()
 
